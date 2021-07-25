a829dda55bcce9bacfd9f7c3fa926420c022c64e
Soner Paycı <soner.payci@turna.com>
2021-07-25 16:07:01 +0300

    Validate Chain and Replace Chain logics are implemented with their tests.

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const CryptoHash = require("./crypto-hash");
 
 class BlockChain {
     constructor() {
@@ -12,6 +13,38 @@ class BlockChain {
         });
         this.chain.push(newBlock);
     }
+
+    ReplaceChain(chain) {
+        if(chain.length < this.chain.length)
+            return;
+
+
+        if(!BlockChain.ValidateChain(chain))
+            return;
+
+        this.chain = chain;
+
+    }
+
+    static ValidateChain(chain) {
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.Genesis()))
+            return false;
+
+        for (let i = 1; i < chain.length; i++){
+            const { timestamp, lastHash, hash, data} = chain[i];
+            const actualLastHash = chain[i-1].hash;
+
+            if (lastHash !== actualLastHash)
+                return false;
+
+            const validatedHash = CryptoHash(timestamp, lastHash, data);
+
+            if(hash !== validatedHash)
+                return false;
+        }
+        return true;
+    }
+
 }
 
 module.exports = BlockChain;
--- tests/blockchain.test.js
+++ tests/blockchain.test.js
@@ -3,7 +3,13 @@ const Block = require('../src/block');
 
 
 describe('BlockChain', () => {
-    const blockChain = new BlockChain();
+    let blockChain, newChain, originalChain;
+
+    beforeEach(() => {
+        blockChain = new BlockChain();
+        newChain = new BlockChain();
+        originalChain = blockChain.chain;
+    });
 
     it('contains a `chain` Array instance', () => {
         expect(blockChain.chain instanceof Array).toBe(true);
@@ -19,4 +25,74 @@ describe('BlockChain', () => {
         expect(blockChain.chain[blockChain.chain.length -1].data).toEqual(newData);
     });
 
+    describe('isValidChain()', () => {
+        beforeEach(() => {
+            blockChain = new BlockChain();
+            blockChain.AddBlock({data:'Bears'});
+            blockChain.AddBlock({data:'Beers'});
+            blockChain.AddBlock({data:'Beatles'});
+        });
+        describe('when the chain does not start with genesis block', () => {
+            it('returns false', () => {
+                blockChain.chain[0] = {data: 'fake-genesis-data'};
+                expect(BlockChain.ValidateChain(blockChain.chain)).toBe(false);
+            });
+        });
+
+        describe('when the chain starts with the genesis block and has multiple blocks', () => {
+            describe('and a lastHash reference has changed', () => {
+               it('returns false', () => {
+                    blockChain.chain[2].lastHash = 'corrupted-hash';
+                    expect(BlockChain.ValidateChain(blockChain.chain)).toBe(false);
+               });
+            });
+
+            describe('and the chain contains a block with an invalid field', () => {
+               it('returns false', () => {
+                   blockChain.chain[2].data = 'some-bad-evil-data';
+                   expect(BlockChain.ValidateChain(blockChain.chain)).toBe(false);
+               });
+            });
+
+            describe('and the chain does not contains a block with an invalid field', () => {
+               it('returns true', () => {
+                   expect(BlockChain.ValidateChain(blockChain.chain)).toBe(true);
+               });
+            });
+        });
+    });
+
+    describe('replaceChain()', () => {
+        describe('when the new chain is not longer', () => {
+            it('does not replace the chain', () => {
+                newChain.chain[0] = {new:'chain'};
+                blockChain.ReplaceChain(newChain.chain);
+                expect(blockChain.chain).toEqual(originalChain);
+            });
+        });
+
+        describe('when the new chain is longer', () => {
+            beforeEach(() => {
+                newChain= new BlockChain();
+                newChain.AddBlock({data:'Bears'});
+                newChain.AddBlock({data:'Beers'});
+                newChain.AddBlock({data:'Beatles'});
+            });
+
+            describe('and the incoming chain is invalid', () => {
+                it('does not replace the chain', () => {
+                    newChain.chain[2].hash = 'fake-hash';
+                    blockChain.ReplaceChain(newChain.chain);
+                    expect(blockChain.chain).toEqual(originalChain);
+                });
+            });
+
+            describe('and the incoming chain is valid', () => {
+                it('replaces the chain', () => {
+                    blockChain.ReplaceChain(newChain.chain);
+                    expect(blockChain.chain).toEqual(newChain.chain);
+                });
+            });
+        });
+    })
 });
